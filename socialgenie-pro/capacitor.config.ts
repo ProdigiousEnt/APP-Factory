800d87258c563a2c8963f28cdb874e60d3704841
Almostcivilized <dennisharrington66@gmail.com>
2026-01-18 13:30:13 -0500

    Delete socialgenie-pro directory

--- socialgenie-pro/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.socialgenie.pro',
-  appName: 'SocialGenie Pro',
-  webDir: 'dist'
-};
-
-export default config;
